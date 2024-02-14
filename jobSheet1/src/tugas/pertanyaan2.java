1f3c9f046c2fb62a7772943dd63b502fdc7cf8b0
aorysan <aryoadiputro@gmail.com>
2024-02-14 14:13:59 +0700

    update fungsi

--- jobSheet1/src/tugas/pertanyaan2.java
+++ jobSheet1/src/tugas/pertanyaan2.java
@@ -6,48 +6,85 @@ public class pertanyaan2 {
         
         Scanner sc = new Scanner(System.in);
 
+        int s = 0, v = 0, t = 0;
+
         System.out.println("apa yang ingin Anda cari ? ");
         System.out.println("1. jarak\n2. kecepatan\n3. waktu");
         String cari = sc.next();
-
+        
         switch (cari) {
             case "1":
-                System.out.print("masukkan kecepatan : ");
-                int v = sc.nextInt();
-                System.out.print("masukkan waktu : ");
-                int t = sc.nextInt();
 
-                int s = v * t;
-                System.out.print("hasilnya adalah : " + (double)s);
+                jarak(t, v);
                 
                 break;
                 
             case "2":
-                System.out.print("masukkan jarak : ");
-                s = sc.nextInt();
-                System.out.print("masukkan waktu : ");
-                t = sc.nextInt();
-
-                v = s / t;
-                System.out.print("hasilnya adalah : " + (double)v);
+                
+                kecepatan(s, t);
                 
                 break;
                 
             case "3":
-                System.out.print("masukkan jarak : ");
-                s = sc.nextInt();
-                System.out.print("masukkan kecepatan : ");
-                v = sc.nextInt();
-
-                t = s / v;
-                System.out.print("hasilnya adalah : " + (double)t);
+                
+                waktu(s, v);
 
                 break;
         
             default:
                 System.out.println("input yang anda masukkan tidak valid");
                 break;
-        }
+            }
+
         sc.close();
+    }
+
+    static int jarak (int t, int v) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukkan kecepatan : ");
+        v = sc.nextInt();
+        System.out.print("masukkan waktu : ");
+        t = sc.nextInt();
+
+        int s = v * t;
+        System.out.print("hasilnya adalah : " + (double)s);
+
+        sc.close();
+
+        return s;
+    }
+    static int kecepatan (int s, int t) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukkan jarak : ");
+        s = sc.nextInt();
+        System.out.print("masukkan waktu : ");
+        t = sc.nextInt();
+
+        int v = s / t;
+        System.out.print("hasilnya adalah : " + (double)v);
+
+        sc.close();
+
+        return s;
+    }
+    static int waktu (int s, int v) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukkan jarak : ");
+        s = sc.nextInt();
+        System.out.print("masukkan kecepatan : ");
+        v = sc.nextInt();
+
+        int t = s / v;
+        System.out.print("hasilnya adalah : " + (double)t);
+
+        sc.close();
+
+        return s;
     }
 }
