4b8d498a6a9e31d01f728eb78ec6fb081bb1abcb
aorysan <aryoadiputro@gmail.com>
2024-05-20 22:17:23 +0700

    update

--- pertemuan14/kuis2/src/dll.java
+++ pertemuan14/kuis2/src/dll.java
@@ -7,23 +7,70 @@ public class dll {
         head = null;
     }
 
+    boolean isEmpty() {
+        return head == null;
+    }
+
     void printTotal() {
         node temp = head;
+        int total[] = new int[20];
 
         System.out.println("\n============================================");
         System.out.println("         Hasil Akhir Perolehan Poin         ");
         System.out.println("============================================");
 
         while (temp != null) {
-            int total = 0;
+
+            int j = 0;
 
             for (int i = 0; i < 21; i++) {
-                total += temp.pembalap.poin[i];
+                total[j] += temp.pembalap.poin[i];
             }
 
             System.out.println("| " + temp.pembalap.nama + "\t\t: " + total + "\t\t     |");
 
             temp = temp.next;
+            j++;
+        }
+
+        node current, index;
+        racer tmp;
+    
+        if (!isEmpty()) {
+            current = head;
+            while (current != null) {
+                index = current.next;
+                int i = 0;
+                while (index != null) {
+                    if (total[i] < total[i+1]) {
+                        tmp = current.pembalap;
+                        current.pembalap = index.pembalap;
+                        index.pembalap = tmp;
+                    }
+                    index = index.next;
+                }
+                current = current.next;
+            }
+            if(!isEmpty()) {
+                node tem = head;
+                
+                System.out.println("===========");
+                System.out.println("Daftar Film");
+                System.out.println("===========");
+                
+                int i = 0;
+
+                while(tem != null) {
+                    System.out.println("ID\t: " + tem.pembalap.nama + total[i]);
+                    System.out.println();
+                    tem = tem.next;
+                    i++;
+                }
+            } else {
+                System.out.println("List kosong");
+            }
+        } else {
+            System.out.println("Linked list masih kosong, tidak dapat mengurutkan data!");
         }
     }
 
